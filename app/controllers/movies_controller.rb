5190ac1a807e1b7adc9974a3cd36bb2931ac9a12
17110298 <mysmartspices@gmail.com>
2016-02-28 19:04:08 +0000

    All Parts Covered

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,7 +4,100 @@ class MoviesController < ApplicationController
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
   
-  def allmovies
+  
+  
+  def deletemovies
+     
+     @all_ratings = Movie.all_ratings
+     
+     
+     if params[:ratings] != nil
+      @movies = Movie.where(:rating => [params[:ratings].keys])
+      
+        if @movies
+            @movies.each do |movie|
+            movie.destroy
+           end
+           flash[:notice] = "Movies with ratings: #{params[:ratings].keys} deleted."
+           
+        end
+        
+      
+     end
+     
+     
+     
+     
+     if params[:Entringmovie] != nil
+      
+      @movie = Movie.find_by_title(params[:Entringmovie].values)
+      
+           if @movie
+           @movie.destroy
+           flash[:notice] = "Movie '#{@movie.title}' deleted."
+           
+           end
+           
+      
+    
+     
+     end
+     
+    
+    
+    
+  end
+  
+  
+  
+  def updates
+   
+   
+  sessions = 0
+
+    if params[:EnteredMovie] != nil
+    
+    # @kuta = Movie.where(:title => [params[:EnteredMovie].values])
+      @movie = Movie.find_by_title(params[:EnteredMovie].values)
+      
+      
+      
+      if @movie
+          
+            if params[:movie].values[0] != ""
+                @movie.update_attributes!(:title => params[:movie].values[0])
+            end
+            if params[:movie].values[1] != ""
+                 @movie.update_attributes!(:rating => params[:movie].values[1])
+            end
+             if params[:movie].values[2] != ""
+                 @movie.update_attributes!(:release_date => params[:movie].values[2])
+            end
+           
+        
+          flash[:notice] = "Movie Informations Updated "
+        
+         
+  
+        
+      else
+          flash[:notice] = "Erroe: Movie Not Found, Try Again " 
+            
+  
+          
+      end
+        
+        
+     sessions = 1
+   
+ 
+  end
+  
+    
+    # puts "asdads #{@rizi[rizibest]}"
+    if sessions ==1 
+    redirect_to movies_path
+    end
     
   end
   
@@ -18,12 +111,88 @@ class MoviesController < ApplicationController
   end
 
   def index
-     @movies =  Movie.all.sort_by { |a| a[params[:sort_by]]}
- end
+  
+     @all_ratings= Movie.all_ratings
+     @hash = Hash[*@all_ratings.map{|key| [key,"1"]}.flatten]
+     
+    if params[:ratings] != nil
+      @hash= params[:ratings]
+      @movies =  Movie.where(:rating => [@hash.keys])
+      session[:ratings] = @hash
+    end
+    
+    if params[:sort_by] != nil
+       
+        if params[:sort_by] == "title"
       
+        @hili = "hilite"
+       elsif params[:sort_by] == "release_date"
+        @hilii = "hilite"
+       end
+        
+        session[:sort] =  params[:sort_by]
+        
+         @movies = Movie.all.sort_by { |a| a[params[:sort_by]]} 
+    end
+     
+     
+     
+     if params[:ratings]== nil && params[:sort_by] == nil
+       @movies = Movie.where(:rating => [@hash.keys])
+       
+     end
+    
+    if (params[:sort_by] == nil || params[:ratings] == nil)
+        if session[:ratings] != nil
+          @hash = session[:ratings]
+        end
+        
+      @movies = Movie.where(:rating => [@hash.keys]).all.sort_by { |a| a[params[:sort_by]]} 
+    end
+      
+    
+   
+  
+   
+   
+    
+    
+    
+  
+    #   @all_ratings= Movie.all_ratings
+       
+    # if params[:ratings] != nil
+    #   @hash= params[:ratings]
+     
+    #   session[:ratings] = @hash
+    # else 
+    
+    
+    #   @hash = Hash[*@all_ratings.map{|key| [key,"1"]}.flatten]
+    #     if session[:ratings] != nil
+    #     @hash = session[:ratings] 
+      
+    #     end
+       
+    # end
+         
+    # if params[:sort_by] == "title"
+    #     @hili = "hilite"
+    # elsif params[:sort_by] == "release_date"
+    #     @hilii = "hilite"
+    # end
+     
+    
+    
+    # @movies = Movie.where(:rating => [session[:ratings].keys]).all.sort_by { |a| a[params[:sort_by]]} 
+   
+     
+    #   puts "prat #{params[:ratings]}  psort #{params[:sort_by]} srat #{session[:ratings]} ssor #{session[:sort_by]} "
+  end
+  
+  
 
   def new
-   
     # default: render 'new' template
   end
 
@@ -32,6 +201,8 @@ class MoviesController < ApplicationController
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
+ 
+  
 
   def edit
     @movie = Movie.find params[:id]
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'movies#index'
-  # get "/movies/:id/allmovies" => "movies#allmovies"
+  get "/movies/updates" => "movies#updates"
+  get "/movies/deletemovies" => "movies#deletemovies"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
